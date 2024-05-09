5da466fc4db73382e6ca97587158adbec3a8a5b3
ena02 <erkembekov.nurzhan@mail.ru>
2024-05-09 21:07:23 +0500

    init commit

---
+++ src/test/java/com/ena/emergencynotificationservice/EmergencyNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ena.emergencynotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
